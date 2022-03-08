642033dee4e983bc9cd8a030a0012c5d95526cd0
kingphisher <kingphisher@RunwhileYouCn.local>
2022-03-08 15:02:00 +0300

    improved cli

--- index.js
+++ index.js
@@ -1,49 +1,8 @@
-var unirest = require("unirest");
+var auth = require('mpesa-auth-token-generator');
 
-if(process.argv.slice(2)[0] === 'production' || process.argv.slice(2)[0] === 'PRODUCTION'){
+var environment = process.argv.slice(2)[0] //Can be either 'production' or 'sandbox'
+var consumerKey = process.argv.slice(2)[1] //Enter consumerKey issued by Daraja
+var consumerSecret = process.argv.slice(2)[2] //Enter consumerSecret issues by daraja
 
-    console.log('Mpesa Bearer Token Generator');
-    console.log('*****************************');
-    console.log('* Fetching Bearer Token For Production');
-    var req = unirest("GET", "https://api.safaricom.co.ke/oauth/v1/generate");
-    let data = process.argv.slice(2)[1]+':'+process.argv.slice(2)[2];
-    
-    let base64string = Buffer.from(data, 'utf-8').toString('base64');
 
-        req.query({
-         "grant_type": "client_credentials"
-        });
-        
-        req.headers({
-         "Authorization": `Basic ${base64string}`
-        });
-        
-        req.end(res => {
-         if (res.error) throw new Error(res.error);
-            console.log("Your Bearer Auth Token Is: "+ res.body.access_token+"\n\n****************");
-        });
-
-}else{
-
-    console.log('Mpesa Bearer Token Generator');
-    console.log('*****************************');
-    console.log('* Fetching Bearer Token For Production');
-
-    var req = unirest("GET", "https://sandbox.safaricom.co.ke/oauth/v1/generate");
-    let data = process.argv.slice(2)[1]+':'+process.argv.slice(2)[2];
-    console.log(data)
-    let base64string = Buffer.from(data, 'base64')
-
-    req.query({
-         "grant_type": "client_credentials"
-        });
-        
-        req.headers({
-         "Authorization": `Basic ${base64string}`
-        });
-        
-        req.end(res => {
-         if (res.error) throw new Error(res.error);
-            console.log("Your Bearer Auth Token Is: "+ res.body.access_token+"\n\n****************");
-        });
-}
+auth.generate(environment,consumerKey,consumerSecret);
